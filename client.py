8370961eea868ea18f110a9d5a6396422d5f5c51
nurulhidayahshkr <nurulhidayahshkr@gmail.com>
2021-12-24 18:57:06 +0000

    first commit

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import signal
+import sys
+
+ClientSocket = socket.socket()
+host = '192.168.0.107'
+port = 8828
+
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(1024)
+print(Response.decode("utf-8"))
+while True:
+    Input = input('Enter the operation and the  number: ')
+
+    if Input == 'exit':
+        break
+    else:
+        ClientSocket.send(str.encode(Input))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode("utf-8"))
+
+ClientSocket.close()
